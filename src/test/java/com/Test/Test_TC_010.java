38fc3390c83d0cb8f1cb4e82661c44eac9c2ad5e
rohan chavan <rohanc64@gmail.com>
2023-10-04 19:24:12 +0530

    Test_tc_010

---
+++ src/test/java/com/Test/Test_TC_010.java
@@ -0,0 +1,13 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+public class Test_TC_010 {
+	
+	@Test
+	public void verifytest_tc_010() {
+		System.out.println("Test_tc_010-Rohan Chavan");
+	}
+		
+	
+}
