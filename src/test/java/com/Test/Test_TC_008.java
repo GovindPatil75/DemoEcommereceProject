3699cff882ed813b84c82f5d7bb7b4306004055f
Swati Dumbre <dswati2903.com>
2023-10-04 11:16:27 +0530

    Test_TC008

---
+++ src/test/java/com/Test/Test_TC_008.java
@@ -0,0 +1,12 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+//Swati Dumbre
+public class Test_TC_008 {
+	
+	@Test
+	public void verify_TC_008() {
+		System.out.println("Test TC 008");
+
+}
+}
