1466c0ff79122980c652731ac92db6975ac02fe0
NilDamodar <damodarn1@gmail.com>
2023-10-04 10:49:08 +0530

    Test_007

---
+++ src/test/java/com/Test/Test_TC_007.java
@@ -0,0 +1,11 @@
+package com.Test;
+import org.testng.annotations.Test;
+          //Nil Damodar//
+public class Test_TC_007 {
+
+		@Test
+		public void verify_TC_007() {
+			System.out.println("Test TC 007");
+	}
+}
+
