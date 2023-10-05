067592902693d7f18e1a1fb2d8ee6d735cc0c003
rohan chavan <rohanc64@gmail.com>
2023-10-05 09:17:52 +0530

    tc_017

---
+++ src/test/java/com/Test/test_tc_017.java
@@ -0,0 +1,11 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+public class test_tc_017 {
+	@Test
+	public void VerifyTest() {
+		System.out.println("Test_TC_017");
+		// resuable class 
+	}
+}
